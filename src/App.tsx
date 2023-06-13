2364eb7143ca288f5a784c8c32db449d34fb6210
Trend20 <enockomondi305@gmail.com>
2023-06-13 19:19:43 +0300

    adding homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import Home from './components/Home';
 import Navbar from './components/shared/Navbar';
+import Homepage from './components/Homepage';
 
 function App():JSX.Element {
   return (
     <div className="app">
       <Navbar />
-      <Home />
+      <Homepage />
     </div>
   );
 }
--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Box, Grid, Button, Typography } from '@mui/material';
 
-const Homepage = () => {
+const Homepage = ():JSX.Element => {
   return (
-    <div>Homepage</div>
+    <Box component={'div'} sx={{ 
+      display:'flex', 
+      flexDirection:'column', 
+      width:'100%', 
+      alignItems:'center', 
+      justifyContent:'center', 
+      padding:'150px' }}>
+      <Typography variant='h1' style={{ fontSize:"48px", fontWeight: 'bold' }}>Get Reminded of Your Valuable Tasks!</Typography> 
+      <Typography>Remind is your watchman ensuring that you deliver on all your critical tasks. </Typography>
+      <Button>Sign up for free</Button>
+    </Box>
   )
 }
 
